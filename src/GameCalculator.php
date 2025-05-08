464ef8702b568793bf23e56ed8e16dae3aed3090
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-08 12:08:30 +0200

    restrict GameCalculator methods visibility

--- src/GameCalculator.php
+++ src/GameCalculator.php
@@ -31,86 +31,6 @@ final class GameCalculator
         $this->places[$this->howManyPlayers()] = 0;
     }
 
-    public function howManyPlayers(): int
-    {
-        return count($this->playersProcess);
-    }
-
-    public function currentPlayer(): int
-    {
-        return $this->currentPlayer;
-    }
-
-    public function nextPlayer(): void
-    {
-        $this->currentPlayer++;
-        if ($this->currentPlayer() == $this->howManyPlayers()) {
-            $this->currentPlayer = 0;
-        }
-    }
-
-    public function isCurrentPlayerNowGettingOutOfPenaltyBox(): bool
-    {
-        return $this->isGettingOutOfPenaltyBox;
-    }
-
-    public function setIsGettingOutOfPenaltyBox(bool $value): void
-    {
-        $this->isGettingOutOfPenaltyBox = $value;
-    }
-
-    public function isCurrentPlayerInPenaltyBox(): bool
-    {
-        return $this->inPenaltyBox[$this->currentPlayer()];
-    }
-
-    public function isPlayerInPenaltyBox(int $player): bool
-    {
-        return $this->inPenaltyBox[$player];
-    }
-
-    public function addCurrentPlayerToPenaltyBox(): void
-    {
-        $this->inPenaltyBox[$this->currentPlayer()] = true;
-    }
-
-    public function pursesBy(int $player): int
-    {
-        return $this->purses[$player];
-    }
-
-    public function increasePursesFor(int $player): void
-    {
-        $this->purses[$player]++;
-    }
-
-    public function currentPlayerPlaces(): int
-    {
-        return $this->places[$this->currentPlayer()];
-    }
-
-    public function increaseCurrentPlayerPlacesBy(int $roll): void
-    {
-        $this->places[$this->currentPlayer()] = $this->currentPlayerPlaces() + $roll;
-        if ($this->currentPlayerPlaces() > 11) {
-            $this->places[$this->currentPlayer()] = $this->currentPlayerPlaces() - 12;
-        }
-    }
-
-    public function isPlayable(): bool
-    {
-        return ($this->howManyPlayers() >= 2);
-    }
-
-    public function isCurrentPlayerGettingOutOfPenaltyBox(): bool
-    {
-        if (!$this->isCurrentPlayerInPenaltyBox()) {
-            return true;
-        }
-
-        return $this->isCurrentPlayerNowGettingOutOfPenaltyBox();
-    }
-
     public function processRoll(int $roll): void
     {
         if ($this->isCurrentPlayerInPenaltyBox()) {
@@ -144,8 +64,88 @@ final class GameCalculator
         $this->nextPlayer();
     }
 
+    public function currentPlayer(): int
+    {
+        return $this->currentPlayer;
+    }
+
+    public function isPlayerInPenaltyBox(int $player): bool
+    {
+        return $this->inPenaltyBox[$player];
+    }
+
+    public function pursesBy(int $player): int
+    {
+        return $this->purses[$player];
+    }
+
+    public function currentPlayerPlaces(): int
+    {
+        return $this->places[$this->currentPlayer()];
+    }
+
+    public function isPlayable(): bool
+    {
+        return ($this->howManyPlayers() >= 2);
+    }
+
+    public function isCurrentPlayerGettingOutOfPenaltyBox(): bool
+    {
+        if (!$this->isCurrentPlayerInPenaltyBox()) {
+            return true;
+        }
+
+        return $this->isCurrentPlayerNowGettingOutOfPenaltyBox();
+    }
+
     public function didPlayerWin(int $player): bool
     {
         return !($this->pursesBy($player) == 6);
+    }
+
+    private function increaseCurrentPlayerPlacesBy(int $roll): void
+    {
+        $this->places[$this->currentPlayer()] = $this->currentPlayerPlaces() + $roll;
+        if ($this->currentPlayerPlaces() > 11) {
+            $this->places[$this->currentPlayer()] = $this->currentPlayerPlaces() - 12;
+        }
+    }
+
+    private function increasePursesFor(int $player): void
+    {
+        $this->purses[$player]++;
+    }
+
+    private function addCurrentPlayerToPenaltyBox(): void
+    {
+        $this->inPenaltyBox[$this->currentPlayer()] = true;
+    }
+
+    private function isCurrentPlayerInPenaltyBox(): bool
+    {
+        return $this->inPenaltyBox[$this->currentPlayer()];
+    }
+
+    private function setIsGettingOutOfPenaltyBox(bool $value): void
+    {
+        $this->isGettingOutOfPenaltyBox = $value;
+    }
+
+    private function isCurrentPlayerNowGettingOutOfPenaltyBox(): bool
+    {
+        return $this->isGettingOutOfPenaltyBox;
+    }
+
+    private function nextPlayer(): void
+    {
+        $this->currentPlayer++;
+        if ($this->currentPlayer() == $this->howManyPlayers()) {
+            $this->currentPlayer = 0;
+        }
+    }
+
+    private function howManyPlayers(): int
+    {
+        return count($this->playersProcess);
     }
 }
